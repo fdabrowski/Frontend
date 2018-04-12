5cfdac16b420d0c14622080e45e4d39e89aad0b6
filip <filip.dabrowski.pg@gmail.com>
2018-04-12 23:20:59 +0200

    add wikipedia result list

--- lab5/app.js
+++ lab5/app.js
@@ -11,7 +11,15 @@ keyups.throttleTime(500).map(function(x){
     console.log(x);
 })
 .subscribe(function(x){
-    $('#results').text(x);
+    $('#results').empty();
+    $('#results').append("<ul id='resultsList'></ul>");
+    if(x.length) {
+        for (i = 1; i < x[1].length; i++) {
+            $("#resultsList").append(
+                "<li>"+x[1][i] +" - <a href=" +x[3][i] +">" +x[3][i] +"</a></li>"
+            );
+        } 
+    }
 })
 
 
